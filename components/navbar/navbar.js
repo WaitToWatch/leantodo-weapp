e0dee63d5aad690a497e627e8515cf89cea82a3c
huangkeqing <huangkeqing@zhuojianchina.com>
2018-03-12 17:20:55 +0800

    practice component(1)

---
+++ components/navbar/navbar.js
@@ -0,0 +1,34 @@
+// pages/list/navbar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    leftContent: {
+      type:String,
+      value:''
+    },
+    centerContent: {
+      type: String,
+      value: ''
+    },
+    rightContent: {
+      type: String,
+      value: ''
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
